97ea27ff38ffb1eff2208798ee5e867f37f96b62
Aaron Van Prooyen <aaron.vanprooyen@ostusa.com>
2016-11-14 13:15:46 -0500

    added auto download of chrome driver

--- googletest.py
+++ googletest.py
@@ -1,6 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import urllib
+import zipfile
+
+# Download and extract chrome driver 
+urllib.request.urlretrieve("https://chromedriver.storage.googleapis.com/2.25/chromedriver_win32.zip", "chromedriver_win32.zip")
+file = zipfile.ZipFile("chromedriver_win32.zip")
+file.extractall()
 
 driver = webdriver.Chrome()
 driver.get("http://www.google.com")
